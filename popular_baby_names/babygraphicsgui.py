53364cd4fa419a1c89bfe43b291dc58c4b9d6e47
An <83592113+An022@users.noreply.github.com>
2021-09-23 14:37:59 -0400

    Update babygraphicsgui.py

--- popular_baby_names/babygraphicsgui.py
+++ popular_baby_names/babygraphicsgui.py
@@ -5,8 +5,7 @@ Sonja Johnson-Yu, Kylie Jue, and Nick Bowman.
 
 This file defines the functions needed to create the GUI for
 the Baby Names project.
-
-You should not modify any of the contents of this file.
+Should not modify any of the contents of this file.
 """
 
 import tkinter
